391c1950e5b0d37777efb350cb19da6119518fd4
michaelpan628 <panmpan@gmail.com>
2018-05-23 12:00:41 +0800

    Udacity project v2

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
 
 # UdacityLogsAnalysis
 ## Purpose
-Analyze date from postgres database
+Analyze date from Postgres database which from a fictional website.
+
+To find out the questions beblows:
+
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
+
+## Database Structure
+
+#### authors
+- id
+- name
+- bio
+
+#### articles
+- id
+- title
+- slug
+- lead
+- body
+- author
+- time
+
+#### log
+- id
+- ip
+- method
+- path
+- status
+- time
 
 ## Requirement
 - Python2
-- psycopg2
-- Postgres Database (Udacity provided)
+- psycopg2 (Python Library)
+- Postgres server & blank database named **"news"**
+- [Fictional data provided by Udacity](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
 
-## execute
-`$ python main.py`
+## Execute
+execute `$ psql -d news -f newsdata.sql`
+
+execute `$ python main.py`
--- main.py
+++ main.py
@@ -1,60 +1,70 @@
+#!/usr/bin/env python
 import psycopg2
 
-def task1(c):
-	print("Task 1\n")
 
-	c.execute("""SELECT articles.title, count(log.id) FROM articles
-		LEFT JOIN log
-		ON log.path LIKE CONCAT('/article/', articles.slug)
-		AND status='200 OK'
-		GROUP BY articles.title
-		ORDER BY count DESC
-		LIMIT 3;""")
+def print_popular_articles(c):
+    print("1. What are the most popular three articles of all time?\n")
 
-	for i in c.fetchall():
-		print('"{}" - {} views'.format(i[0], i[1]))
+    c.execute("""SELECT articles.title, count(log.id) FROM articles
+        LEFT JOIN log
+        ON log.path=CONCAT('/article/', articles.slug)
+        AND status='200 OK'
+        GROUP BY articles.title
+        ORDER BY count DESC
+        LIMIT 3;""")
 
-def task2(c):
-	print("\nTask 2\n")
+    for i in c.fetchall():
+        print('"{}" - {} views'.format(i[0], i[1]))
 
-	c.execute("""SELECT authors.name, sum(view_count) AS view_sum
-		from authors, (SELECT articles.author AS author_id, count(log.id) AS view_count
-		    FROM articles
-		    LEFT JOIN log
-		    ON log.path LIKE CONCAT('/article/', articles.slug)
-		    AND status='200 OK'
-		    GROUP BY articles.author) AS VC
-		WHERE authors.id=author_id
-		GROUP BY authors.name
-		ORDER BY view_sum DESC;""")
 
-	for i in c.fetchall():
-		print('{} - {} views'.format(i[0], i[1]))
+def print_author_ranking(c):
+    print("\n2. Who are the most popular article authors of all time?\n")
 
-def task3(c):
-	print("\nTask 3\n")
+    c.execute("""SELECT authors.name, sum(view_count) AS view_sum
+        from authors,
+        (SELECT articles.author AS author_id, count(log.id) AS view_count
+            FROM articles
+            LEFT JOIN log
+            ON log.path=CONCAT('/article/', articles.slug)
+            AND status='200 OK'
+            GROUP BY articles.author) AS VC
+        WHERE authors.id=author_id
+        GROUP BY authors.name
+        ORDER BY view_sum DESC;""")
 
-	c.execute("""SELECT success.date, (CAST(fail.count AS FLOAT)/CAST(success.count AS FLOAT))*100 AS fail_chance
-		FROM (SELECT CAST(time AS DATE) AS date, count(*) FROM log
-		WHERE status='200 OK'
-		GROUP BY date) AS success, (SELECT CAST(time AS DATE) AS date, count(*) FROM log
-		WHERE status='404 NOT FOUND'
-		GROUP BY date) AS fail
-		WHERE success.date=fail.date;""")
+    for i in c.fetchall():
+        print('{} - {} views'.format(i[0], i[1]))
 
-	for i in c.fetchall():
-		print("{} - {}% errors".format(i[0].strftime("%b %d,%Y"), i[1]))
+
+def print_error_percentage(c):
+    print("\n3. On which days did more than 1% of requests lead to errors?\n")
+
+    c.execute("""SELECT success.date,
+            (error.count/(CAST(success.count AS FLOAT)+error.count)*100)
+        FROM (SELECT CAST(time AS DATE) AS date, count(*) FROM log
+            WHERE status='200 OK'
+            GROUP BY date) AS success,
+        (SELECT CAST(time AS DATE) AS date, count(*) FROM log
+            WHERE status='404 NOT FOUND'
+            GROUP BY date) AS error
+        WHERE success.date=error.date
+        AND (error.count/(CAST(success.count AS FLOAT)+error.count)*100)>1;""")
+
+    for i in c.fetchall():
+        print("{0:%B %d, %Y} - {1:.2f}% errors".format(i[0], i[1]))
+
 
 def main():
-	DBNAME = "news"
-	db = psycopg2.connect(database=DBNAME)
-	c = db.cursor()
+    DBNAME = "news"
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
 
-	task1(c)
-	task2(c)
-	task3(c)
+    print_popular_articles(c)
+    print_author_ranking(c)
+    print_error_percentage(c)
 
-	db.close()
+    db.close()
+
 
 if __name__ == "__main__":
-	main()
+    main()
